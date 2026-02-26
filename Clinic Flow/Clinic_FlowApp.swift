ad2c3a301a42827c74d25e3e7070655de56f0df9
gimz12 <kumalillankoon12@gmail.com>
2026-02-26 18:29:57 +0530

    Initial Commit

---
+++ Clinic Flow/Clinic_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clinic_FlowApp.swift
+//  Clinic Flow
+//
+//  Created by Kumal Illankoon on 2026-02-26.
+//
+
+import SwiftUI
+
+@main
+struct Clinic_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
